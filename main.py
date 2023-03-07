1f181086348ab4bd5890694863cc350f8054d355
Michael Wekesa <wekesa360@outlook.com>
2023-03-07 23:46:16 +0300

    update signup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, auth, db
 from flask import Flask, jsonify, request, session
 import os
 
-cred = credentials.Certificate('./cred.json')
+cred = credentials.Certificate('./key.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://fundamentals-demo-default-rtdb.asia-southeast1.firebasedatabase.app'
 })
@@ -11,17 +11,31 @@ firebase_admin.initialize_app(cred, {
 app = Flask(__name__)
 app.secret_key = os.urandom(54)
 
+app.config['FIREBASE_ADMIN_APP'] = firebase_admin.get_app()
 
 @app.route('/api/signup', methods=['POST'])
 def signup():
     email = request.json['email']
     password = request.json['password']
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    gender = request.json['gender']
+
     try:
         user = auth.create_user(
             email=email,
-            password=password
+            password=password,
+            display_name=first_name
         )
         session['user'] = user.uid
+        db.reference(f'users/{user.uid}').set(
+            {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': user.email,
+            'gender': gender
+            }
+        )
         return jsonify({'message': 'Account created successfully.'}), 201
     except Exception as e:
         return jsonify({'message': f'There was an issue creating your account: {e}'}), 40
@@ -31,9 +45,14 @@ def signup():
 def login():
     email = request.json['email']
     password = request.json['password']
+
+    app = app.config['FIREBASE_ADMIN_APP']
+    auth = firebase_admin.auth
+    
+    user = auth.get_user_by_email(email, password)
     try:
-        user = auth.get_user_by_email(email)
-        auth.sign_in_with_email_and_password(email, password)
+        decoded_token = auth.verify_id_token(user.id_token)
+        user = auth.get_user(decoded_token['uid'])
         session['user'] = user.uid
         return jsonify({'message': 'Login successful'}), 200
     except Exception as e:
@@ -46,14 +65,19 @@ def logout():
     return jsonify({'message': 'Logout successful.'}), 200
 
 
-@app.route('api/profile', methods=['GET'])
+@app.route('/api/profile', methods=['GET'])
 def profile():
     if 'user' in session:
         user = auth.get_user(session['user'])
         user_data = db.reference(f'users/{user.uid}').get()
-        return jsonify({'email': user.email, 'name': user_data['name']}), 200
+        return jsonify({'email': user.email, 
+                        'first_name': user_data['first_name'],
+                        'last_name': user_data['last_name'],
+                        'gender': user_data['gender'] }), 200
     else:
         return jsonify({'message': 'You are not logged in'}), 401
     
-    
-        
+
+
+if __name__ == '__main__':
+    app.run()
